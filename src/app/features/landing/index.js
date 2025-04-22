b7c61d546c111bbc5ff511c52deb8e2ef28e901c
mohd ruman <mohd.ruman.82@gmail.com>
2025-04-22 20:13:28 +0530

    Initial commit : ready to code file structure

---
+++ src/app/features/landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Hero from './components/Hero.jsx';
+
+const LandingPage = () => {
+  return (
+    <>
+      <h1>Lvl 2 : LandingPage index</h1>
+      <Hero/>
+    </>
+  )
+}
+
+export default LandingPage
